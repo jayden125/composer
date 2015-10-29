70c775460b58c92a5eb8a07a03e5f00fe649f63e
jayden125 <yagotelles@hotmail.com>
2015-10-29 20:23:27 -0200

    teste

---
+++ app/views/topics/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @topic, :id, :subject, :user_id, :category_id, :created_at, :updated_at
---
+++ spec/factories/topics.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :topic do
+    subject "MyString"
+user nil
+category nil
+  end
+
+end
